2843a541d7239a558cece65da2509508149eb58d
fadiyahputri <fadiyah089putri@gmail.com>
2022-11-15 11:59:45 +0700

    first

---
+++ app/Http/Controllers/Cloud.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Cloud extends Controller
+{
+    public function index() {
+        return view ('index');
+    }
+
+    public function guru() {
+        return view ('guru');
+    }
+
+    public function matpel() {
+        return view ('matpel');
+    }
+    
+    public function cloud() {
+        return view ('cloud');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Cloud;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/guru', [Cloud::class, 'guru']);
+
+Route::get('/matpel', [Cloud::class, 'matpel']);
+
+Route::get('/cloud', [Cloud::class, 'cloud']);
